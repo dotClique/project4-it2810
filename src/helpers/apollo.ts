063074df7d7f26b4ccc6244eb5143b3aaf6e911d
Ulrik Røsby <ulrik@rosby.no>
2021-11-13 14:13:30 +0100

    fix: separate the graphql url to dotenv

--- src/helpers/apollo.ts
+++ src/helpers/apollo.ts
@@ -1,6 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { getEnv } from "./utils";
 
-const apiUrl = "http://it2810-24.idi.ntnu.no/api/graphql";
+//const apiUrl = "http://it2810-24.idi.ntnu.no/api/graphql";
+const apiUrl = getEnv("graphqlUrl");
 
 const client = new ApolloClient({
   uri: apiUrl,
