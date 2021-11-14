654ec3cf871293cf284d83d458940be6b537ec04
Birk Gustav Samson Stoveland <bgstovel@stud.ntnu.no>
2021-11-14 17:16:27 +0100

    feat: created a basic pagecontainer

--- src/components/LogOutButton/index.tsx
+++ src/components/LogOutButton/index.tsx
@@ -2,6 +2,7 @@ import { Button } from "react-native-paper";
 import * as React from "react";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { ParamList } from "src/types/navigation";
+import { Text } from "react-native";
 type Props = { navigation: StackNavigationProp<ParamList> };
 export default function LogOutButton({ navigation }: Props) {
   return (
@@ -11,7 +12,7 @@ export default function LogOutButton({ navigation }: Props) {
         navigation.reset({ routes: [{ name: "Home" }] });
       }}
     >
-      Log Out
+      <Text>Log Out</Text>
     </Button>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,29 +5,32 @@ import { ParamList } from "src/types/navigation";
 import { StackScreenProps } from "@react-navigation/stack";
 import { Button, Headline } from "react-native-paper";
 import styles from "./styles";
+import PageContainer from "components/PageContainer";
 
 type Props = StackScreenProps<ParamList, "Home">;
 
 export default function Home({ navigation }: Props) {
   return (
     <View style={styles.container}>
-      <Headline>Hello</Headline>
-      <Button
-        mode={"contained"}
-        onPress={() => {
-          navigation.reset({ routes: [{ name: "MovieGroupsPage" }] });
-        }}
-      >
-        MovieGroupsPage
-      </Button>
-      <Button
-        mode={"contained"}
-        onPress={() => {
-          navigation.reset({ routes: [{ name: "FavoriteGroupsPage" }] });
-        }}
-      >
-        FavoriteMovieGroupsPage
-      </Button>
+      <PageContainer navigation={navigation} title={"Home"} footer={<View></View>}>
+        <Headline>Hello</Headline>
+        <Button
+          mode={"contained"}
+          onPress={() => {
+            navigation.reset({ routes: [{ name: "MovieGroupsPage" }] });
+          }}
+        >
+          MovieGroupsPage
+        </Button>
+        <Button
+          mode={"contained"}
+          onPress={() => {
+            navigation.reset({ routes: [{ name: "FavoriteGroupsPage" }] });
+          }}
+        >
+          FavoriteMovieGroupsPage
+        </Button>
+      </PageContainer>
     </View>
   );
 }
