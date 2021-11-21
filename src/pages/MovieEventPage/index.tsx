c45670ad01ec20c033765c81e27ab6119de2a138
Ulrik Røsby <ulrik@rosby.no>
2021-11-21 18:12:50 +0100

    feat: add movie event page

--- src/pages/MovieEventPage/index.tsx
+++ src/pages/MovieEventPage/index.tsx
@@ -1,33 +1,58 @@
-import * as React from "react";
-import { StyleSheet, View } from "react-native";
-
-import { ParamList } from "types/navigation";
 import { StackScreenProps } from "@react-navigation/stack";
-import { Button, Headline, Subheading } from "react-native-paper";
+import * as React from "react";
+import { View } from "react-native";
+import { Button, Card, Divider } from "react-native-paper";
+import PageContainer from "src/components/PageContainer";
+import { ParamList } from "types/navigation";
+import MovieEventTextItem from "../../components/MovieEventTextItem";
+import { useMovieEvent } from "./utils";
+
 type Props = StackScreenProps<ParamList, "MovieEventPage">;
 
-export default function MovieEventPage({ route, navigation }: Props) {
-  const { MovieEventId } = route.params;
+export default function MovieEventPage({ route }: Props) {
+  const { MovieEventId: movieEventId } = route.params;
+  // const movieEventId = "ckvftqcsa2136alpcvwggpbmm";
+  const { isParticipant, movieData, joinEvent, leaveEvent } = useMovieEvent(movieEventId);
   return (
-    <View style={styles.container}>
-      <Headline>MovieEventPage</Headline>
-      <Subheading>ID: {MovieEventId}</Subheading>
-      <Button
-        mode={"contained"}
-        onPress={() => {
-          navigation.goBack();
-        }}
-      >
-        Go back!
-      </Button>
-    </View>
+    <PageContainer
+      title="temp"
+      footer={
+        <View>
+          {movieData && isParticipant ? (
+            <Button mode="contained" onPress={() => leaveEvent()}>
+              Leave Event
+            </Button>
+          ) : (
+            <Button mode="contained" onPress={() => joinEvent()}>
+              Join Event
+            </Button>
+          )}
+        </View>
+      }
+    >
+      <Card>
+        <Card.Content>
+          <View>
+            <MovieEventTextItem title="Description" body={movieData.description} />
+            <Divider />
+            <MovieEventTextItem title="Location" body={movieData.location} />
+            <Divider />
+            <MovieEventTextItem
+              title="Datetime"
+              body={movieData.date.replace("T", " ").replace("Z", "").slice(0, -4)}
+            />
+            <Divider />
+            <MovieEventTextItem
+              title={"Status"}
+              body={
+                movieData && isParticipant
+                  ? "You are a participant of this event"
+                  : "You are not a participant of this event"
+              }
+            />
+          </View>
+        </Card.Content>
+      </Card>
+    </PageContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
